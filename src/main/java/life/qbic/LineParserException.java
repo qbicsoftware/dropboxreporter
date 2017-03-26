78bb0bed5a8d64b817ded177f1a8f8594ea88716
sven <sven.fillinger@mailbox.org>
2017-03-26 20:14:36 +0200

    Slim LineParserException

--- src/main/java/life/qbic/LineParserException.java
+++ src/main/java/life/qbic/LineParserException.java
@@ -1,25 +1,14 @@
 package life.qbic;
 
-import javax.sound.sampled.Line;
 
 /**
  * Created by sven on 3/21/17.
  */
 public class LineParserException extends RuntimeException {
 
-    public LineParserException() {
-        super();
-    }
 
     public LineParserException(String message){
         super(message);
     }
 
-    public LineParserException(Throwable t){
-        super(t);
-    }
-
-    public LineParserException(String message, Throwable t){
-        super(message, t);
-    }
 }
